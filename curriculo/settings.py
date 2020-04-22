cc56628b812cdcd1f92b4b59ddf915137e30df39
Danie-Lomezo <daniel.ubletech@gmail.com>
2020-04-21 23:30:26 -0300

    asd

--- curriculo/settings.py
+++ curriculo/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
     'appcurriculo'
 ]
 
@@ -123,6 +122,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_URL = '/static/'
 PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
