625a124e00682027d3d41e96183d50b6f64c90f2
Danie-Lomezo <daniel.ubletech@gmail.com>
2020-04-21 22:20:35 -0300

    master

--- curriculo/settings.py
+++ curriculo/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'curriculo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'curriculo',
+        'NAME': 'd9s9oehgtn1k36',
         # 'NAME': os.path.join(BASE_DIR, ''),
-        'USER': 'd9s9oehgtn1k36',
+        'USER': 'vjdzbvikpuaahn',
         'PASSWORD': '2934b1a44167021f557fd2900e60fea7c33eb505edf30a0f73f13f2732b47ce0',
         'HOST': 'ec2-52-71-85-210.compute-1.amazonaws.com',
         '5432': '', # 8000 is default
