7c4b6bd3e0f5db879b283df4175406fca30d10ba
Danie-Lomezo <daniel.ubletech@gmail.com>
2020-04-23 23:51:46 -0300

    asd

--- emailpak/email.py
+++ emailpak/email.py
@@ -11,48 +11,50 @@ class mail(object):
 
     def enviaemail(self, titulo, corpomensagem, destinatarios):
 
-                
-        # conexão com os servidores do google
-        smtp_ssl_host = 'smtp.gmail.com'
-        smtp_ssl_port = 465
+            try:         
+                # conexão com os servidores do google
+                smtp_ssl_host = 'smtp.gmail.com'
+                smtp_ssl_port = 465
 
-        # username ou email para logar no servidor
-        username = 'daniel.lomezohdm@gmail.com'
+                # username ou email para logar no servidor
+                username = 'daniel.lomezohdm@gmail.com'
 
-        password = 'daniellomezo008090'
+                password = 'daniellomezo008090'
 
-        emaildeenvio = username
-        destinatarios = destinatarios, 'daniel.ubletech@gmail.com'
-
-
-
-        # a biblioteca email possuí vários templates
-        # para diferentes formatos de mensagem
-        # neste caso usaremos MIMEText para enviar
-        # somente texto
+                emaildeenvio = username
+                destinatarios = destinatarios, 'daniel.ubletech@gmail.com'
 
 
-        message = MIMEMultipart()
-        message.attach(MIMEText(corpomensagem, 'plain'))
-        message['subject'] = titulo
-        message['from'] = emaildeenvio
-        message['to'] = ', '.join(destinatarios)
-        #config = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-        #attachment = open(config, "rb")
 
-        part = MIMEBase('application', 'octet-stream')
-        #part.set_payload((attachment).read())
-        #encoders.encode_base64(part)
-        #part.add_header('Content-Disposition', "attachment; filename={}".format(caminho.replace('logs/', '')))
-        #print(caminho)
-        message.attach(part)
+                # a biblioteca email possuí vários templates
+                # para diferentes formatos de mensagem
+                # neste caso usaremos MIMEText para enviar
+                # somente texto
 
-        # conectaremos de forma segura usando SSL
-        server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
 
-        # para interagir com um servidor externo precisaremos
-        # fazer login nele
-        server.login(username, password)
-        server.sendmail(emaildeenvio, destinatarios, message.as_string())
-        server.quit()
+                message = MIMEMultipart()
+                message.attach(MIMEText(corpomensagem, 'plain'))
+                message['subject'] = titulo
+                message['from'] = emaildeenvio
+                message['to'] = ', '.join(destinatarios)
+                #config = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+                #attachment = open(config, "rb")
 
+                part = MIMEBase('application', 'octet-stream')
+                #part.set_payload((attachment).read())
+                #encoders.encode_base64(part)
+                #part.add_header('Content-Disposition', "attachment; filename={}".format(caminho.replace('logs/', '')))
+                #print(caminho)
+                message.attach(part)
+
+                # conectaremos de forma segura usando SSL
+                server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+
+                # para interagir com um servidor externo precisaremos
+                # fazer login nele
+                server.login(username, password)
+                server.sendmail(emaildeenvio, destinatarios, message.as_string())
+                server.quit()
+
+            except:
+                print('erro')
