ece92c9172f9df89e8e4b09838d9e72f393ef0c5
daniel-lomezo <daniel.ubletech@gmail.com>
2020-05-17 16:28:13 -0300

    set

--- curriculo/settings.py
+++ curriculo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'bbvx7x*4r0q0d5owb)!990lq^8z&_@)(xe9ra0_7iqp6m9&or$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['joincurriculo.herokuapp.com', 'localhost']
 
 
 # Application definition
