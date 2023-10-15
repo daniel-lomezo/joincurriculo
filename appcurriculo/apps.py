2199e6541c6aefa379058e42e3138e43a2abfa1f
Daniel Lomezo <daniel.ubletech@gmail.com>
2023-10-15 16:16:39 -0300

    teste

--- appcurriculo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppcurriculoConfig(AppConfig):
-    name = 'appcurriculo'
--- appcurriculo/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class FormTelegram(forms.Form):
-	nome = forms.CharField(label='Seu nome', max_length=100)
-	email = forms.EmailField()
-	id_telegram = forms.CharField(label='Seu ID Telegrm ou Número', max_length=80)
-	messagem = forms.CharField(widget=forms.Textarea)
-
